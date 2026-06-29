e43a59057702ba3bd23a5f9664111b8f7c567aa9
aziz-boujaada <azizboujaada87@gmail.com>
2026-06-29 15:42:26 +0100

    retuiel resto

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Rituel/', 
+
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
